f8eb0ec2f48478a282d604cd8beebae0955fa1c3
André Sanches Souto <andresanchessouto@gmail.com>
2023-07-30 18:54:27 -0300

    Update Register.java

--- JavaApplication82/src/javaapplication82/Register.java
+++ JavaApplication82/src/javaapplication82/Register.java
@@ -18,10 +18,10 @@ public class Register {
     public int yearBornUser;
     protected double cpfUser;
     
-    public void registerPainel(){
+    protected void registerPainel(){
         
         System.out.print("--Insert here yours informations--\nName user: ");
-        nameUser = sc.nextLine();
+        setNameUser(sc.nextLine());
         
         System.out.print("Year you've born: ");
         yearBornUser =  sc.nextInt();
@@ -30,6 +30,38 @@ public class Register {
         cpfUser =  sc.nextDouble();
         
         System.out.println("\n"+ nameUser +", you will be redirected to include your personalities information to help us on your plan of exercices");
+        perfilUser();
         
     }
+    
+    protected void perfilUser(){
+        
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String name) {
+        this.nameUser = name;
+    }
+
+    public int getYearBornUser() {
+        return yearBornUser;
+    }
+
+    public void setYearBornUser(int yearBornUser) {
+        this.yearBornUser = yearBornUser;
+    }
+
+    public double getCpfUser() {
+        return cpfUser;
+    }
+
+    public void setCpfUser(double cpfUser) {
+        this.cpfUser = cpfUser;
+    }
+    
+    
+    
 }
